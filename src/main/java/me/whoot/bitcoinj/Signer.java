90ccd848ec8101f8d28192fae95ccf849b54cad3
Kosta Korenkov <7r0ggy@gmail.com>
2014-07-25 01:27:06 +0400

    Signer now accepts derivation path to choose proper key.
    Other changes:
    - logging (slf4j + log4j binding)
    - key seed made constant to ease testing

--- src/main/java/me/whoot/bitcoinj/Signer.java
+++ src/main/java/me/whoot/bitcoinj/Signer.java
@@ -2,33 +2,53 @@ package me.whoot.bitcoinj;
 
 import com.google.bitcoin.core.ECKey;
 import com.google.bitcoin.core.Sha256Hash;
+import com.google.bitcoin.core.Utils;
+import com.google.bitcoin.crypto.ChildNumber;
 import com.google.bitcoin.crypto.DeterministicKey;
+import com.google.bitcoin.crypto.HDUtils;
 import com.google.bitcoin.wallet.DeterministicKeyChain;
+import com.google.common.collect.ImmutableList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.security.SecureRandom;
 
 @Path("/signer")
 public class Signer {
 
-    private static DeterministicKey key = new DeterministicKeyChain(new SecureRandom()).getWatchingKey();
+    public static final Logger log = LoggerFactory.getLogger(Signer.class);
+
+    private static final byte[] ENTROPY = Sha256Hash.create("don't use a string seed like this in real life".getBytes()).getBytes();
+    private static DeterministicKeyChain keyChain;
+
+    static {
+        keyChain = new DeterministicKeyChain(ENTROPY, "", 1389353062L);
+        keyChain.setLookaheadSize(2);
+    }
 
     @GET
     @Path("/xpub")
     @Produces(MediaType.TEXT_PLAIN)
     public String getXpub() {
-        return key.serializePubB58();
+        return keyChain.getWatchingKey().serializePubB58();
     }
 
-    @GET
+    @POST
     @Path("/sign")
     @Produces(MediaType.TEXT_PLAIN)
-    public byte[] getIt(String hexSighash) {
+    public byte[] sign(@FormParam("sighash") String hexSighash, @FormParam("keypath") String keyPath) {
+        log.info("Signing sighash={} with key by path {}", hexSighash, keyPath);
         Sha256Hash sighash = new Sha256Hash(hexSighash);
+        ImmutableList<ChildNumber> path = ImmutableList.copyOf(HDUtils.parsePath(keyPath));
+
+        DeterministicKey key = keyChain.getKeyByPath(path, true);
+        log.info("Key: {}", key);
+
         ECKey.ECDSASignature signature = key.sign(sighash);
-        return signature.encodeToDER();
+        byte[] sig = signature.encodeToDER();
+        log.info("Done. Signature: {}", Utils.HEX.encode(sig));
+
+        return sig;
     }
 }
